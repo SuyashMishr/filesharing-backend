c487f3dd9056b2651bdd0c977bca58754a5545df
Suyash Mishra <143060685+SuyashMishr@users.noreply.github.com>
2025-05-17 15:06:37 +0530

    Update upload.js

--- utils/upload.js
+++ utils/upload.js
@@ -10,10 +10,11 @@ import multer from "multer";
 import path from "path";
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, 'uploads'),
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); // Make sure this folder exists
+  },
   filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, uniqueSuffix + path.extname(file.originalname));
-  }
+    cb(null, Date.now() + path.extname(file.originalname)); // e.g., 1234567890.pdf
+  },
 });
 export default multer({ storage });
