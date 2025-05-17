b5470fe7c341052954530263352947216444f746
Suyash Mishra <143060685+SuyashMishr@users.noreply.github.com>
2025-05-17 14:28:35 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,31 @@
 import express from 'express';
-
-import { uploadImage, downloadImage } from '../controller/image-controller.js';
-import upload from '../utils/upload.js';
+import File from '../models/file.js';
+import multer from 'multer';
 
 const router = express.Router();
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, 'uploads/'),
+  filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`)
+});
 
-router.post('/upload', upload.single('file'), uploadImage);
-router.get('/file/:fileId', downloadImage);
+const upload = multer({ storage });
+
+router.post("/upload", upload.single('file'), async (req, res) => {
+  try {
+    const file = new File({
+      filename: req.file.filename,
+      path: req.file.path,
+      size: req.file.size,
+    });
+
+    await file.save();
+
+    res.redirect("https://file-sharing-frontend-six.vercel.app/success");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Upload failed");
+  }
+});
 
 export default router;
