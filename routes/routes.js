a2845022226d0b20eff36163e99770eb76c49200
Suyash Mishra <143060685+SuyashMishr@users.noreply.github.com>
2025-06-26 09:39:13 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, 'uploads/'),
-  filename: (req, file, cb) => cb(null, ${Date.now()}-${file.originalname})
+  filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`)
 });
 
 const upload = multer({ storage });
